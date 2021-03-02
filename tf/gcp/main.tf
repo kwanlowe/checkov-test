8595dded03e1cf9a6dcecac01b579fc7f3772094
Kwan Lowe <kwan@digitalhermit.com>
2021-03-01 23:26:03 -0500

    Use variable for client network.

--- tf/gcp/main.tf
+++ tf/gcp/main.tf
@@ -4,9 +4,16 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+
+variable "client_external_ip" {
+  type = string
+  description = "This is the external IP address of the client generating the infrastructure."
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
+  tags = ["jumpoff"]
 
   boot_disk {
     initialize_params {
@@ -26,3 +33,17 @@ resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
+
+
+resource "google_compute_firewall" "fw-allow-ssh" {
+  name = "fw-allow-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  target_tags = ["jumpoff"]
+  source_ranges = [var.client_external_ip]
+}
+
+
